a601a7d6f70cdf45af55a206b9a04424bc7c56c5
Olivia Brode-Roger <nibr@mit.edu>
2023-11-16 12:40:03 -0800

    More configs

--- lua/nibrivia/set.lua
+++ lua/nibrivia/set.lua
@@ -15,5 +15,14 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-
 vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+-- vim.opt.isfname:append('@-@')
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
